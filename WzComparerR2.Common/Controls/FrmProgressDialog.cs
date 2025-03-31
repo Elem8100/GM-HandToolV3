1a8a64636132b927401ed27c0170b2d30c06082b
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2025-03-31 22:54:11 +0800

    Add files via upload

---
+++ WzComparerR2.Common/Controls/FrmProgressDialog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevComponents.DotNetBar;
+
+namespace WzComparerR2.Controls
+{
+    public partial class FrmProgressDialog : DevComponents.DotNetBar.Office2007Form
+    {
+        public FrmProgressDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string Message
+        {
+            get { return this.labelX1.Text; }
+            set { this.labelX1.Text = value; }
+        }
+
+        public int Progress
+        {
+            get { return this.progressBarX1.Value; }
+            set { this.progressBarX1.Value = value; }
+        }
+
+        public int ProgressMin
+        {
+            get { return this.progressBarX1.Minimum; }
+            set { this.progressBarX1.Minimum = value; }
+        }
+
+        public int ProgressMax
+        {
+            get { return this.progressBarX1.Maximum; }
+            set { this.progressBarX1.Maximum = value; }
+        }
+
+        public string FullMessage { get; set; }
+
+        private void labelX1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Clipboard.SetText(this.FullMessage ?? this.Message);
+                ToastNotification.Show(this, "클립보드에 복사되었습니다.", 1000, eToastPosition.TopCenter);
+            }
+        }
+    }
+}
