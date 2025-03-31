2f759f38ef5725647d35c5698a177b98e941d078
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2025-03-31 22:36:33 +0800

    Add files via upload

---
+++ WzComparerR2.Common/CharaSim/GearType.cs
@@ -0,0 +1,543 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WzComparerR2.CharaSim
+{
+    public enum GearType
+    {
+        body = 0,
+        head = 1,
+        face = 2,
+        hair = 3,
+        hair2 = 4,
+        face2 = 5,
+        hair3 = 6,
+
+        /// <summary>
+        /// Face Accessory 101
+        /// </summary>
+        faceAccessory = 101,
+        /// <summary>
+        /// Eye Accessory 102
+        /// </summary>
+        eyeAccessory = 102,
+        /// <summary>
+        /// Earrings 103
+        /// </summary>
+        earrings = 103,
+        /// <summary>
+        /// Pendant 112
+        /// </summary>
+        pendant = 112,
+        /// <summary>
+        /// Belt 113
+        /// </summary>
+        belt = 113,
+        /// <summary>
+        /// Medal 114
+        /// </summary>
+        medal = 114,
+        /// <summary>
+        /// Shoulder 115
+        /// </summary>
+        shoulderPad = 115,
+        /// <summary>
+        /// Hat 100
+        /// </summary>
+        cap = 100,
+        /// <summary>
+        /// Cape 110
+        /// </summary>
+        cape = 110,
+        /// <summary>
+        /// Top 104
+        /// </summary>
+        coat = 104,
+        /// <summary>
+        /// Dragon Hat 194
+        /// </summary>
+        dragonMask = 194,
+        /// <summary>
+        /// Dragon Pendant 195
+        /// </summary>
+        dragonPendant = 195,
+        /// <summary>
+        /// Dragon Wing Accessory 196
+        /// </summary>
+        dragonWings = 196,
+        /// <summary>
+        /// Dragon Tail Accessory 197
+        /// </summary>
+        dragonTail = 197,
+        /// <summary>
+        /// Gloves 108
+        /// </summary>
+        glove = 108,
+        /// <summary>
+        /// Outfit 105
+        /// </summary>
+        longcoat = 105,
+        /// <summary>
+        /// Mechanic Engine 161
+        /// </summary>
+        machineEngine = 161,
+        /// <summary>
+        /// Mechanic Arm 162
+        /// </summary>
+        machineArms = 162,
+        /// <summary>
+        /// Mechanic Leg 163
+        /// </summary>
+        machineLegs = 163,
+        /// <summary>
+        /// Mechanic Frame 164
+        /// </summary>
+        machineBody = 164,
+        /// <summary>
+        /// Mechanic Transistor 165
+        /// </summary>
+        machineTransistors = 165,
+        /// <summary>
+        /// Android 166
+        /// </summary>
+        android = 166,
+        /// <summary>
+        /// Mechanical Heartz 167
+        /// </summary>
+        machineHeart = 167,
+        /// <summary>
+        /// Pocket Item 116
+        /// </summary>
+        pocket = 116,
+        /// <summary>
+        /// Badge 118
+        /// </summary>
+        badge = 118,
+        /// <summary>
+        /// Emblem / Power Source 119
+        /// </summary>
+        emblem = 119,
+        powerSource = 119020,
+        /// <summary>
+        /// Bottom 106
+        /// </summary>
+        pants = 106,
+        /// <summary>
+        /// Ring 111
+        /// </summary>
+        ring = 111,
+        /// <summary>
+        /// Shield 109
+        /// </summary>
+        shield = 109,
+        /// <summary>
+        /// Soul Shield 1098xxx
+        /// </summary>
+        soulShield = 1098,
+        /// <summary>
+        /// Demon Aegis 1099xxx
+        /// </summary>
+        demonShield = 1099,
+        /// <summary>
+        /// Shoes 107
+        /// </summary>
+        shoes = 107,
+        /// <summary>
+        /// Shining Rod 1212
+        /// </summary>
+        shiningRod = 1212,
+        /// <summary>
+        /// Bladecaster 1213
+        /// </summary>
+        tuner = 1213,
+        /// <summary>
+        /// Whispershot 1214
+        /// </summary>
+        breathShooter = 1214,
+        /// <summary>
+        /// Soul Shooter 122
+        /// </summary>
+        soulShooter = 122,
+        /// <summary>
+        /// Desperado 123
+        /// </summary>
+        desperado = 123,
+        /// <summary>
+        /// Whip Blade 124
+        /// </summary>
+        energySword = 124,
+        /// <summary>
+        /// Memorial Staff 1252
+        /// </summary>
+        memorialStaff = 1252,
+        /// <summary>
+        /// Beast Tamer Scepter 1259
+        /// </summary>
+        magicStick = 1259,
+        /// <summary>
+        /// Psy-limiter
+        /// </summary>
+        espLimiter = 126,
+        /// <summary>
+        /// Chain 127
+        /// </summary>
+        chain2 = 127,
+        /// <summary>
+        /// Lucent Gauntlet 128
+        /// </summary>
+        magicGauntlet = 128,
+        /// <summary>
+        /// Ritual Fan 129
+        /// </summary>
+        handFan = 129,
+        /// <summary>
+        /// One-handed Sword 130
+        /// </summary>
+        ohSword = 130,
+        /// <summary>
+        /// One-handed Axe 131
+        /// </summary>
+        ohAxe = 131,
+        /// <summary>
+        /// One-handed Blunt Weapon 132
+        /// </summary>
+        ohBlunt = 132,
+        /// <summary>
+        /// Dagger 133
+        /// </summary>
+        dagger = 133,
+        /// <summary>
+        /// Katara 134
+        /// </summary>
+        katara = 134,
+        /// <summary>
+        /// Magic Arrow 135_00
+        /// </summary>
+        magicArrow = 135200,
+        /// <summary>
+        /// Card 135_10
+        /// </summary>
+        card = 135210,
+        /// <summary>
+        /// Medallions 135_20
+        /// </summary>
+        heroMedal = 135220,
+        /// <summary>
+        /// Rosary 135_21
+        /// </summary>
+        rosario = 135221,
+        /// <summary>
+        /// Iron Chain 135_22
+        /// </summary>
+        chain = 135222,
+        /// <summary>
+        /// Magic Book (Fire,Poison) 135_23
+        /// </summary>
+        book1 = 135223,
+        /// <summary>
+        /// Magic Book (Ice,Lightning) 135_24
+        /// </summary>
+        book2 = 135224,
+        /// <summary>
+        /// Magic Book (Bishop) 135_25
+        /// </summary>
+        book3 = 135225,
+        /// <summary>
+        /// Arrow Fletching 135_26
+        /// </summary>
+        bowMasterFeather = 135226,
+        /// <summary>
+        /// Bow Thimble 135_27
+        /// </summary>
+        crossBowThimble = 135227,
+        /// <summary>
+        /// Dagger Scabbard 135_28
+        /// </summary>
+        shadowerSheath = 135228,
+        /// <summary>
+        /// Charm 135_29
+        /// </summary>
+        nightLordPoutch = 135229,
+        /// <summary>
+        /// Core 135_30
+        /// </summary>
+        box = 135230,
+        /// <summary>
+        /// Orb 135_40
+        /// </summary>
+        orb = 135240,
+        /// <summary>
+        /// Dragon Essence 135_50
+        /// </summary>
+        novaMarrow = 135250,
+        /// <summary>
+        /// Soul Ring 135_60
+        /// </summary>
+        soulBangle = 135260,
+        /// <summary>
+        /// Magnum 135_70
+        /// </summary>
+        mailin = 135270,
+        /// <summary>
+        /// Kodachi 135_80
+        /// </summary>
+        kodachi = 135280,
+        /// <summary>
+        /// Leaf 135_81
+        /// </summary>
+        leaf = 135281,
+        /// <summary>
+        /// Fist 135_82
+        /// </summary>
+        boxingClaw = 135282,
+        /// <summary>
+        /// Kodachi 135_83
+        /// </summary>
+        kodachi2 = 135283,
+        /// <summary>
+        /// Leaf 135_84
+        /// </summary>
+        leaf2 = 135284,
+        /// <summary>
+        /// 拳天 135_86
+        /// </summary>
+        boxingSky = 135286,
+        /// <summary>
+        /// Wrist Band 135_90
+        /// </summary>
+        viperWristband = 135290,
+        /// <summary>
+        /// Far Sight 135_91
+        /// </summary>
+        captainSight = 135291,
+        /// <summary>
+        /// Powder Keg 135_92
+        /// </summary>
+        cannonGunPowder = 135292,
+        /// <summary>
+        /// Mass 135_93
+        /// </summary>
+        aranPendulum = 135293,
+        /// <summary>
+        /// Document 135_94
+        /// </summary>
+        evanPaper = 135294,
+        /// <summary>
+        /// Magic Marble 135_95
+        /// </summary>
+        battlemageBall = 135295,
+        /// <summary>
+        /// Arrowhead 135_96
+        /// </summary>
+        wildHunterArrowHead = 135296,
+        /// <summary>
+        /// Jewel 135_97
+        /// </summary>
+        cygnusGem = 135297,
+        /// <summary>
+        /// Powder Keg 135_98
+        /// </summary>
+        cannonGunPowder2 = 135298,
+        /// <summary>
+        /// Controller 135300
+        /// </summary>
+        controller = 135300,
+        /// <summary>
+        /// Fox Marble 135310
+        /// </summary>
+        foxPearl = 135310,
+        /// <summary>
+        /// Chess Piece 135320
+        /// </summary>
+        chess = 135320,
+        /// <summary>
+        /// Warp Forge 135330
+        /// </summary>
+        transmitter = 135330,
+        /// <summary>
+        /// Charge 135340
+        /// </summary>
+        explosivePill = 135340,
+        /// <summary>
+        /// Lucent Wings 135350
+        /// </summary>
+        magicWing = 135350,
+        /// <summary>
+        /// Abyssal Path 135360
+        /// </summary>
+        pathOfAbyss = 135360,
+        /// <summary>
+        /// Relic 135370x
+        /// </summary>
+        relic = 135370,
+        /// <summary>
+        /// Fan Tassel 135380x
+        /// </summary>
+        fanTassel = 135380,
+        /// <summary>
+        /// Bladebinder 135400x
+        /// </summary>
+        bracelet = 135400,
+        /// <summary>
+        /// Weapon Belt 135401x
+        /// </summary>
+        weaponBelt = 135401,
+        /// <summary>
+        /// Ornament 135402x
+        /// </summary>
+        ornament = 135402,
+        /// <summary>
+        /// Hex Seeker 135403x
+        /// </summary>
+        hexSeeker = 135403,
+        /// <summary>
+        /// Cane 136
+        /// </summary>
+        cane = 136,
+        /// <summary>
+        /// Wand 137
+        /// </summary>
+        wand = 137,
+        /// <summary>
+        /// Staff 138
+        /// </summary>
+        staff = 138,
+        /// <summary>
+        /// Bare Hands 139
+        /// </summary>
+        barehand = 139,
+        /// <summary>
+        /// Two-handed Sword 140
+        /// </summary>
+        thSword = 140,
+        /// <summary>
+        /// 拳封 140_3xxx
+        /// </summary>
+        boxingCannon = 1403,
+        /// <summary>
+        /// Chakram 140_4xxx
+        /// </summary>
+        chakram = 1404,
+        /// <summary>
+        /// Two-handed Axe 141
+        /// </summary>
+        thAxe = 141,
+        /// <summary>
+        /// Two-handed Blunt Weapon 142
+        /// </summary>
+        thBlunt = 142,
+        /// <summary>
+        /// Spear 143
+        /// </summary>
+        spear = 143,
+        /// <summary>
+        /// Polearm 144
+        /// </summary>
+        polearm = 144,
+        /// <summary>
+        /// Bow 145
+        /// </summary>
+        bow = 145,
+        /// <summary>
+        /// Crossbow 146
+        /// </summary>
+        crossbow = 146,
+        /// <summary>
+        /// Claw 147
+        /// </summary>
+        throwingGlove = 147,
+        /// <summary>
+        /// Knuckle 148
+        /// </summary>
+        knuckle = 148,
+        /// <summary>
+        /// Gun 149
+        /// </summary>
+        gun = 149,
+        /// <summary>
+        /// Herbalism Tool 150
+        /// </summary>
+        shovel = 150,
+        /// <summary>
+        /// Mining Tool 151
+        /// </summary>
+        pickaxe = 151,
+        /// <summary>
+        /// Dual Bowguns 152
+        /// </summary>
+        dualBow = 152,
+        /// <summary>
+        /// Hand Cannon 153
+        /// </summary>
+        handCannon = 153,
+        /// <summary>
+        /// Katana 154
+        /// </summary>
+        katana = 154,
+        /// <summary>
+        /// Fan 155
+        /// </summary>
+        fan = 155,
+        /// <summary>
+        /// Heavy Sword 156
+        /// </summary>
+        swordZB = 156,
+        /// <summary>
+        /// Long Sword 157
+        /// </summary>
+        swordZL = 157,
+        /// <summary>
+        /// Arm Cannon 158
+        /// </summary>
+        gauntletBuster = 158,
+        /// <summary>
+        /// Ancient Bow 159
+        /// </summary>
+        ancientBow = 159,
+        /// <summary>
+        /// Bit 168
+        /// </summary>
+        bit = 168,
+        /// <summary>
+        /// Cash Weapon 170
+        /// </summary>
+        cashWeapon = 170,
+        /// <summary>
+        /// Weapon -1
+        /// </summary>
+        weapon = -1,
+        /// <summary>
+        /// Secondary Weapon -1
+        /// </summary>
+        subWeapon = -2,
+        /// <summary>
+        /// Totem 120
+        /// </summary>
+        totem = 120,
+        /// <summary>
+        /// Pet Equipment 180
+        /// </summary>
+        petEquip = 180,
+        /// <summary>
+        /// Tamed Monster 190
+        /// </summary>
+        taming = 190,
+        /// <summary>
+        /// Tamed Monster 191
+        /// </summary>
+        saddle = 191,
+        /// <summary>
+        /// Tamed Monster 193
+        /// </summary>
+        taming2 = 193,
+        /// <summary>
+        /// Tamed Monster 198
+        /// </summary>
+        tamingChair = 198,
+        /// <summary>
+        /// Tamed Monster 199
+        /// </summary>
+        taming3 = 199
+    }
+}
---
+++ WzComparerR2.Common/CharaSim/Item.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WzComparerR2.WzLib;
+
+namespace WzComparerR2.CharaSim
+{
+    public class Item : ItemBase
+    {
+        public Item()
+        {
+            this.Props = new Dictionary<ItemPropType, long>();
+            this.Specs = new Dictionary<ItemSpecType, long>();
+            this.CoreSpecs = new Dictionary<ItemCoreSpecType, Wz_Node>();
+            this.AddTooltips = new List<int>();
+            this.Recipes = new List<int>();
+        }
+
+        public int Level { get; set; }
+        public int? DamageSkinID { get; set; }
+        public string ConsumableFrom { get; set; }
+        public string EndUseDate { get; set; }
+        public string SamplePath { get; set; }
+
+        public List<GearLevelInfo> Levels { get; internal set; }
+
+        public Dictionary<ItemPropType, long> Props { get; private set; }
+        public Dictionary<ItemSpecType, long> Specs { get; private set; }
+        public Dictionary<ItemCoreSpecType, Wz_Node> CoreSpecs { get; private set; }
+        public List<int> AddTooltips { get; internal set; } // Additional Tooltips
+        public List<int> Recipes { get; private set; }
+
+        public bool Cash
+        {
+            get { return GetBooleanValue(ItemPropType.cash); }
+        }
+
+        public bool TimeLimited
+        {
+            get { return GetBooleanValue(ItemPropType.timeLimited); }
+        }
+
+        public bool GetBooleanValue(ItemPropType type)
+        {
+            return this.Props.TryGetValue(type, out long value) && value != 0;
+        }
+
+        public static Item CreateFromNode(Wz_Node node, GlobalFindNodeFunction findNode)
+        {
+            Item item = new Item();
+            int value;
+            if (node == null
+                || !Int32.TryParse(node.Text, out value)
+                && !((value = node.Text.IndexOf(".img")) > -1 && Int32.TryParse(node.Text.Substring(0, value), out value)))
+            {
+                return null;
+            }
+            item.ItemID = value;
+
+            // in msn the node could be UOL.
+            if (node.Value is Wz_Uol)
+            {
+                if ((node = node.ResolveUol()) == null)
+                {
+                    return item;
+                }
+            }
+
+            Wz_Node infoNode = node.FindNodeByPath("info");
+            if (infoNode != null)
+            {
+                foreach (Wz_Node subNode in infoNode.Nodes)
+                {
+                    switch (subNode.Text)
+                    {
+                        case "icon":
+                            if (subNode.Value is Wz_Uol || subNode.Value is Wz_Png)
+                            {
+                                item.Icon = BitmapOrigin.CreateFromNode(subNode, findNode);
+                            }
+                            break;
+
+                        case "iconRaw":
+                            if (subNode.Value is Wz_Uol || subNode.Value is Wz_Png)
+                            {
+                                item.IconRaw = BitmapOrigin.CreateFromNode(subNode, findNode);
+                            }
+                            break;
+
+                        case "sample":
+                            if (subNode.Value is Wz_Uol || subNode.Value is Wz_Png)
+                            {
+                                item.Sample = BitmapOrigin.CreateFromNode(subNode, findNode);
+                            }
+                            break;
+
+                        case "lv":
+                            item.Level = Convert.ToInt32(subNode.Value);
+                            break;
+
+                        case "damageSkinID":
+                            item.DamageSkinID = Convert.ToInt32(subNode.Value);
+                            break;
+
+                        case "consumableFrom":
+                            item.ConsumableFrom = Convert.ToString(subNode.Value);
+                            break;
+
+                        case "endUseDate":
+                            item.EndUseDate = Convert.ToString(subNode.Value);
+                            break;
+
+                        case "samplePath":
+                            item.SamplePath = Convert.ToString(subNode.Value);
+                            break;
+
+                        case "exp":
+                            foreach (Wz_Node subNode2 in subNode.Nodes)
+                            {
+                                ItemPropType type2;
+                                if (Enum.TryParse("exp_" + subNode2.Text, out type2))
+                                {
+                                    try
+                                    {
+                                        item.Props.Add(type2, Convert.ToInt32(subNode2.Value));
+                                    }
+                                    finally
+                                    {
+                                    }
+                                }
+                            }
+                            break;
+
+                        case "level": //可升级信息
+                            Wz_Node levelInfo = subNode.Nodes["info"];
+                            item.Levels = new List<GearLevelInfo>();
+                            if (levelInfo != null)
+                            {
+                                for (int i = 1; ; i++)
+                                {
+                                    Wz_Node levelInfoNode = levelInfo.Nodes[i.ToString()];
+                                    if (levelInfoNode != null)
+                                    {
+                                        GearLevelInfo info = GearLevelInfo.CreateFromNode(levelInfoNode);
+                                        int lv;
+                                        Int32.TryParse(levelInfoNode.Text, out lv);
+                                        info.Level = lv;
+                                        item.Levels.Add(info);
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+
+                            Wz_Node levelCase = subNode.Nodes["case"];
+                            if (levelCase != null)
+                            {
+                                int probTotal = 0;
+                                foreach (Wz_Node caseNode in levelCase.Nodes)
+                                {
+                                    int prob = caseNode.Nodes["prob"].GetValueEx(0);
+                                    probTotal += prob;
+                                    for (int i = 0; i < item.Levels.Count; i++)
+                                    {
+                                        GearLevelInfo info = item.Levels[i];
+                                        Wz_Node caseLevel = caseNode.Nodes[info.Level.ToString()];
+                                        if (caseLevel != null)
+                                        {
+                                            //desc
+                                            Wz_Node caseHS = caseLevel.Nodes["hs"];
+                                            if (caseHS != null)
+                                            {
+                                                info.HS = caseHS.GetValue<string>();
+                                            }
+
+                                            //随机技能
+                                            Wz_Node caseSkill = caseLevel.Nodes["Skill"];
+                                            if (caseSkill != null)
+                                            {
+                                                foreach (Wz_Node skillNode in caseSkill.Nodes)
+                                                {
+                                                    int id = skillNode.Nodes["id"].GetValueEx(-1);
+                                                    int level = skillNode.Nodes["level"].GetValueEx(-1);
+                                                    if (id >= 0 && level >= 0)
+                                                    {
+                                                        info.Skills[id] = level;
+                                                    }
+                                                }
+                                            }
+
+                                            //装备技能
+                                            Wz_Node equipSkill = caseLevel.Nodes["EquipmentSkill"];
+                                            if (equipSkill != null)
+                                            {
+                                                foreach (Wz_Node skillNode in equipSkill.Nodes)
+                                                {
+                                                    int id = skillNode.Nodes["id"].GetValueEx(-1);
+                                                    int level = skillNode.Nodes["level"].GetValueEx(-1);
+                                                    if (id >= 0 && level >= 0)
+                                                    {
+                                                        info.EquipmentSkills[id] = level;
+                                                    }
+                                                }
+                                            }
+                                            info.Prob = prob;
+                                        }
+                                    }
+                                }
+
+                                foreach (var info in item.Levels)
+                                {
+                                    info.ProbTotal = probTotal;
+                                }
+                            }
+                            item.Props.Add(ItemPropType.level, 1);
+                            break;
+
+                        case "addTooltip":
+                            if (subNode.Nodes.Count > 0)
+                            {
+                                foreach (Wz_Node tooltipNode in subNode.Nodes)
+                                {
+                                    item.AddTooltips.Add(Convert.ToInt32(tooltipNode.Value));
+                                }
+                            }
+                            else
+                            {
+                                item.AddTooltips.Add(Convert.ToInt32(subNode.Value));
+                            }
+                            break;
+
+                        default:
+                            ItemPropType type;
+                            if (Enum.TryParse(subNode.Text, out type))
+                            {
+                                try
+                                {
+                                    item.Props.Add(type, Convert.ToInt64(subNode.Value));
+                                }
+                                catch (Exception)
+                                {
+                                }
+                            }
+                            break;
+                    }
+                }
+            }
+
+            Wz_Node specNode = node.FindNodeByPath("spec");
+            if (specNode != null)
+            {
+                foreach (Wz_Node subNode in specNode.Nodes)
+                {
+                    if (subNode.Text == "recipe")
+                    {
+                        if (subNode.Value == null && subNode.Nodes.Count > 0)
+                        {
+                            foreach (var recipeNode in subNode.Nodes)
+                            {
+                                item.Recipes.Add(recipeNode.GetValue<int>());
+                            }
+                        }
+                        else
+                        {
+                            item.Recipes.Add(subNode.GetValue<int>());
+                        }
+                    }
+                    else if(Enum.TryParse(subNode.Text, out ItemSpecType type))
+                    {
+                        try
+                        {
+                            item.Specs.Add(type, Convert.ToInt64(subNode.Value));
+                        }
+                        finally
+                        {
+                        }
+                    }
+                }
+            }
+
+            Wz_Node coreSpecNode = node.FindNodeByPath("corespec");
+            if (coreSpecNode != null)
+            {
+                item.Props.Remove(ItemPropType.tradeBlock);
+                foreach (Wz_Node subNode in coreSpecNode.Nodes)
+                {
+                    ItemCoreSpecType type;
+                    if (Enum.TryParse(subNode.Text, out type))
+                    {
+                        item.CoreSpecs.Add(type, subNode);
+                    }
+                }
+                if (item.CoreSpecs.ContainsKey(ItemCoreSpecType.Ctrl_addMission))
+                {
+                    List<ItemCoreSpecType> removeSpecs = item.CoreSpecs.Keys.Where(k => k != ItemCoreSpecType.Ctrl_addMission).ToList();
+                    foreach (ItemCoreSpecType type in removeSpecs)
+                    {
+                        item.CoreSpecs.Remove(type);
+                    }
+                }
+            }
+            return item;
+        }
+
+    }
+}
